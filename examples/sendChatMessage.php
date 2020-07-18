da5a17fd95a91806e9587ad5a8f29af747268289
Mohammad Sadman <41444793+kaneki666@users.noreply.github.com>
2020-07-18 15:00:34 +0600

    Update sendChatMessage.php

--- examples/sendChatMessage.php
+++ examples/sendChatMessage.php
@@ -3,6 +3,6 @@
 if(!empty($_POST)) {
     $message = $_POST['message'];
     $redis = new Redis();
-    $redis->pconnect('REDIS_HOST'); // REDIS_HOST hast to be the same as in BBB_REDIS_HOST
-    $redis->publish('REDIS_CHANNEL', $message); // REDIS_CHANNEL hast to be the same as in BBB_REDIS_CHANNEL
+    $redis->pconnect('redis'); // REDIS_HOST hast to be the same as in BBB_REDIS_HOST
+    $redis->publish('chat', $message); // REDIS_CHANNEL hast to be the same as in BBB_REDIS_CHANNEL
     echo "Message published\n";
