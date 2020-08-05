83a0e548b7d27be4d36b1c0606920182506da056
Mohammad Sadman <41444793+kaneki666@users.noreply.github.com>
2020-08-05 13:09:30 +0600

    Update stream.py

--- stream.py
+++ stream.py
@@ -80,7 +80,8 @@ def bbb_browser():
     browser.find_element_by_id('message-input').send_keys("This meeting is streamed : %s" % args.target)
     browser.find_element_by_id('message-input').send_keys("hello sadman: %s" % args.target)
     browser.find_elements_by_css_selector('[aria-label="Send message"]')[0].click()
-    browser.find_element_by_id('tippy-83').click()
+    browser.find_element_by_class_name('button--Z2dosza sm--Q7ujg default--Z19H5du button--Z1ops0C fullScreenButton--Z1bf0vj').click()
+     browser.find_elements_by_css_selector('[aria-label="Make sadman fullscreen"]')[0].click()
     
     if args.chat:
         browser.execute_script("document.querySelector('[aria-label=\"User list\"]').parentElement.style.display='none';")
